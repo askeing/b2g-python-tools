60fa4a6c11c38530397e934ac1df8fb3db2e9584
askeing <askeing@gmail.com>
2013-08-12 17:40:46 +0800

    Added generate_download_list_in_view.py for TW-CI autoflash work

---
+++ generate_download_list_in_view.py
@@ -0,0 +1,218 @@
+# -*- coding: utf-8 -*-
+
+from optparse import OptionParser
+from xml.etree import ElementTree
+from xml.dom.minidom import parseString
+from datetime import datetime
+import urllib
+import urllib2
+import re
+
+'''
+Author: Askeing (askeing@gmail.com)
+
+Jenkins API
+{DL_URL_PATH}/api/xml?depth=2&tree=name,url,jobs[name,lastSuccessfulBuild[id,description,url,artifacts[displayPath,relativePath]]]
+'''
+
+
+def main():
+    # Added parser
+    parser = OptionParser()
+    parser.add_option('-d', '--debug',
+                      action='store_true', dest='enable_debug',
+                      default=False,
+                      help='Display debug info. Default=False')
+    parser.add_option('-s', '--https',
+                      action='store_true', dest='enable_https',
+                      default=False,
+                      help='Enable https. Default=False')
+    parser.add_option('-a', '--address',
+                      action='store', type='string', dest='address',
+                      default='localhost',
+                      help='The Jenkins host address. Default=localhost')
+    parser.add_option('-p', '--port',
+                      action='store', type='int', dest='port',
+                      help='The Jenkins host port.')
+    parser.add_option('-v', '--view',
+                      action='store', type='string', dest='view',
+                      default='All',
+                      help='The View\'s name in Jenkins. Default=All')
+    parser.add_option('-o', '--ouput-html',
+                      action='store', type='string', dest='output',
+                      default='out.html',
+                      help='The html output file.')
+    (options, args) = parser.parse_args()
+
+    # Setup Jenkins Server's URL
+    protocol = 'http'
+    if options.enable_https:
+        protocol = 'https'
+    if options.port is not None:
+        host = '%s:%s' % (options.address, str(options.port))
+    else:
+        host = options.address
+    view = urllib.pathname2url(options.view)
+
+    url = '%s://%s/view/%s/api/xml?depth=2&tree=name,url,jobs[name,lastSuccessfulBuild[id,description,url,artifacts[displayPath,relativePath]]]' % (protocol, host, view)
+    if options.enable_debug:
+        print 'Enable https:', options.enable_https
+        print 'Host: %s' % host
+        print 'View:', options.view
+        print 'URL:', url
+        print
+
+    # enable HTML output
+    enable_html = False
+    if options.output is not None:
+        enable_html = True
+
+    # Retrieve info from Jenkins Server
+    response = urllib2.urlopen(url)
+    test_input = response.read()
+    if options.enable_debug:
+        print test_input
+        print
+
+    # Parse the output of API
+    root = ElementTree.fromstring(test_input)
+
+    # print list view name
+    root_name = root.find('name').text
+    root_url = root.find('url').text
+    print 'DL_NAME=%s' % root_name
+    print 'DL_URL=%s' % root_url
+    now = datetime.now()
+    print 'DL_DATE=%s' % now.strftime('%a %d %b %Y %I:%M:%S').replace(' ', '_')
+    if enable_html:
+        output_root_node = ElementTree.Element('div', attrib={'id': root_name})
+        output_div_node = ElementTree.SubElement(output_root_node, 'div', attrib={'class': 'DL_info DL_name'})
+        output_div_node.text = root_name
+        output_div_node = ElementTree.SubElement(output_root_node, 'div', attrib={'class': 'DL_info DL_url'})
+        output_span_node = ElementTree.SubElement(output_div_node, 'span', attrib={'class': 'key DL_url_key'})
+        output_span_node.text = 'URL: '
+        output_span_node = ElementTree.SubElement(output_div_node, 'span', attrib={'class': 'value DL_url_value'})
+        output_span_node.text = root_url
+        output_div_node = ElementTree.SubElement(output_root_node, 'div', attrib={'class': 'DL_info DL_date'})
+        output_span_node = ElementTree.SubElement(output_div_node, 'span', attrib={'class': 'key DL_date_key'})
+        output_span_node.text = 'Date: '
+        output_span_node = ElementTree.SubElement(output_div_node, 'span', attrib={'class': 'value DL_date_value'})
+        output_span_node.text = now.strftime('%a, %d %b %Y %I:%M:%S')
+        output_br_node = ElementTree.SubElement(output_root_node, 'br')
+
+    # find all jobs
+    idx_job = 0
+    jobs = root.findall('job')
+    for job in jobs:
+        print ''
+        job_name = ''
+        # print job name
+        job_name_node = job.find('name')
+        if job_name_node is not None:
+            job_name = job_name_node.text
+
+            ### break for emulator build
+            emulator_pattern = re.compile('^B2G\..*\.emulator$')
+            emulator_result = emulator_pattern.match(job_name)
+            if emulator_result is not None:
+                print '#Emulator:', emulator_result.group(0)
+                continue
+
+            print 'DL_%s_JOB_NAME=%s' % (idx_job, job_name)
+            if enable_html:
+                output_table_node = ElementTree.SubElement(output_root_node, 'table', attrib={'id': job_name, 'class': 'job_table'})
+                output_tr_node = ElementTree.SubElement(output_table_node, 'tr')
+                output_th_node = ElementTree.SubElement(output_tr_node, 'th', attrib={'class': 'job_name', 'colspan': '2'})
+                output_th_node.text = job_name
+
+        # get last build's info
+        last_build = job.find('lastSuccessfulBuild')
+
+        # get last build id
+        id_node = last_build.find('id')
+        if id_node is not None:
+            id = id_node.text
+            print 'DL_%s_ID=%s' % (str(idx_job), id)
+            if enable_html:
+                output_tr_node = ElementTree.SubElement(output_table_node, 'tr')
+                output_td_node = ElementTree.SubElement(output_tr_node, 'td', attrib={'class': 'key'})
+                output_td_node.text = 'Build ID'
+                output_td_node = ElementTree.SubElement(output_tr_node, 'td', attrib={'class': 'value build_id'})
+                output_td_node.text = id
+
+        # get last build description
+        desc_node = last_build.find('description')
+        if desc_node is not None:
+            desc = desc_node.text.replace(' ', ',')
+            print 'DL_%s_DESC=%s' % (str(idx_job), desc)
+            if enable_html:
+                output_tr_node = ElementTree.SubElement(output_table_node, 'tr')
+                output_td_node = ElementTree.SubElement(output_tr_node, 'td', attrib={'class': 'key'})
+                output_td_node.text = 'Build Description'
+                output_td_node = ElementTree.SubElement(output_tr_node, 'td', attrib={'class': 'value build_description'})
+                output_td_node.text = desc
+
+        # get last build url
+        url_node = last_build.find('url')
+        if url_node is not None:
+            url = url_node.text
+            print 'DL_%s_URL=%s' % (str(idx_job), url)
+            if enable_html:
+                output_tr_node = ElementTree.SubElement(output_table_node, 'tr')
+                output_td_node = ElementTree.SubElement(output_tr_node, 'td', attrib={'class': 'key'})
+                output_td_node.text = 'Build URL'
+                output_td_node = ElementTree.SubElement(output_tr_node, 'td', attrib={'class': 'value build_url'})
+                output_td_node.text = url
+
+        # get artifacts
+        idx_artifact = 0
+        if enable_html and len(last_build.findall('artifact')) > 0:
+            output_tr_node = ElementTree.SubElement(output_table_node, 'tr')
+            output_td_node = ElementTree.SubElement(output_tr_node, 'td', attrib={'class': 'key'})
+            output_td_node.text = 'Artifacts'
+            output_td_node = ElementTree.SubElement(output_tr_node, 'td', attrib={'class': 'value build_artifacts'})
+            output_ul_node = ElementTree.SubElement(output_td_node, 'ul')
+        for artifact in last_build.findall('artifact'):
+            artifact_display_path_node = artifact.find('displayPath')
+            artifact_relative_path_node = artifact.find('relativePath')
+            if artifact_display_path_node is not None and artifact_relative_path_node is not None:
+                artifact_display_path = artifact_display_path_node.text
+                artifact_relative_path = artifact_relative_path_node.text
+
+                ### Gecko, Gaia, and B2G full image pattern
+                gecko_pattern = re.compile('^b2g-.*\.android-arm\.tar\.gz$')
+                gecko_result = gecko_pattern.match(artifact_display_path)
+                gaia_pattern = re.compile('^gaia\.zip$')
+                gaia_result = gaia_pattern.match(artifact_display_path)
+                b2g_image_pattern = re.compile('^' + job_name + '\.zip$')
+                b2g_image_result = b2g_image_pattern.match(artifact_display_path)
+
+                if gecko_result is not None:
+                    print 'DL_%s_ARTIFACT_GECKO=%s' % (str(idx_job), artifact_relative_path)
+                elif gaia_result is not None:
+                    print 'DL_%s_ARTIFACT_GAIA=%s' % (str(idx_job), artifact_relative_path)
+                elif b2g_image_result is not None:
+                    print 'DL_%s_ARTIFACT_B2G_IMG=%s' % (str(idx_job), artifact_relative_path)
+                else:
+                    continue
+                #print 'DL_%s_ARTIFACT_%s=%s' % (str(idx_job), str(idx_artifact), artifact_relative_path)
+                if enable_html:
+                    output_li_node = ElementTree.SubElement(output_ul_node, 'li')
+                    output_a_node = ElementTree.SubElement(output_li_node, 'a', attrib={'class': 'artifact link', 'href': url + 'artifact/' + artifact_relative_path})
+                    output_a_node.text = artifact_display_path
+            #idx_artifact = idx_artifact + 1
+
+        output_br_node = ElementTree.SubElement(output_root_node, 'br')
+        idx_job = idx_job + 1
+    print ''
+    print 'DL_SIZE=%s' % (idx_job ,)
+
+    if enable_html:
+        f = open(options.output, 'w')
+        xml = ElementTree.tostring(output_root_node)
+        xml_dom = parseString(xml)
+        f.write(xml_dom.toprettyxml())
+        f.close()
+
+if __name__ == '__main__':
+    main()
